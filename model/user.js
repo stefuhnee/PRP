d4b75b8a350960c1d73a25623115d8b9e39abfb9
Ruben Sarkisyan <rubens24@uw.edu>
2016-07-09 16:07:35 -0700

    updated bcrypt requirement

--- model/user.js
+++ model/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const secret = process.env.SECRET || 'changeme';
 const jwt = require('jsonwebtoken');
 

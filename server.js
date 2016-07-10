0c1d613eea45265a6932128dc8db5d048c613466
Dan Stineback <dstineback@gmail.com>
2016-07-10 12:05:45 -0700

    updated server.js with error codes and added mongoose and db. we will need to add auth routes, and any routes that are made later

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const dbPort = process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
+mongoose.connect(dbPort);
 
 app.use(express.static(__dirname + '/build'));
+
+app.use((err, req, res, next) =>{
+  res.status(500).json({message: err.message});
+  next(err);
+});
+app.use((req, res)=>{
+  res.status(404).json({message: 'not found'});
+});
 
 app.listen(8080, () => {
   console.log('server is running on 8080');

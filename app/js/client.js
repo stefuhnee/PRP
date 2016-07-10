240f39e18dbe045a5b21e9da39f87126719f0646
Dan Stineback <dstineback@gmail.com>
2016-07-10 11:18:25 -0700

    added template to the app.config and required in routes for client.js. Will need to add the controller to the config once they are made.

--- app/js/client.js
+++ app/js/client.js
@@ -1,1 +1,44 @@
 'use strict';
+const angular = require('angular');
+const ngRoute = require('angular-route');
+const app = angular.module('BucketListApp', [ngRoute]);
+
+require('./controllers')(app);
+require('./directives')(app);
+require('./services')(app);
+
+app.config(function($routeProvider){
+  $routeProvider.when('/',{
+
+  })
+  .when('/blog-admin', {
+    templateUrl: './views/partials/blog-admin.html',
+    controller: '',
+    controllerAS: ''
+  })
+  .when('/blog', {
+    templateUrl: './views/partials/blog.html',
+    controller: '',
+    controllerAS: ''
+  })
+  .when('/login', {
+    templateUrl: './views/partials/login.html',
+    controller: '',
+    controllerAS: ''
+  })
+  .when('/profile-admin', {
+    templateUrl: './views/partials/profile-admin.html',
+    controller: '',
+    controllerAS: ''
+  })
+  .when('/profile', {
+    templateUrl: './views/partials/profile.html',
+    controller: '',
+    controllerAS: ''
+  })
+  .when('/signup', {
+    templateUrl: './views/partials/signup.html',
+    controller: '',
+    controllerAS: ''
+  });
+});

0e42a97558132b9e97018c8029a53852c1ad37e2
Stefanie Hansen <stefuhnee@gmail.com>
2016-07-10 19:45:51 -0700

    added scaffolding coe for all controllers, sass tasks for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const clean = require('gulp-clean');
+const sass = require('gulp-sass');
 
 const paths = {
   js: __dirname + '/app/**/*.js',
@@ -11,7 +12,7 @@ const paths = {
 };
 
 gulp.task('clean', ()=>{
-  return gulp.src('./build/*', {read:false})
+  return gulp.src('./build/**/*', {read:false})
     .pipe(clean());
 });
 
@@ -23,6 +24,16 @@ gulp.task('copy-html', ['clean'], ()=>{
 gulp.task('copy-css', ['clean'], ()=>{
   return gulp.src(paths.css)
     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./app/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/**/*.scss', ['sass']);
 });
 
 gulp.task('bundle', ['clean'], ()=>{
@@ -46,9 +57,9 @@ gulp.task('bundle:test', () => {
 });
 
 gulp.task('watch', ()=>{
-  gulp.watch('./app/*', ['build']);
+  gulp.watch('./app/*', ['build', 'sass:watch']);
 });
 
-gulp.task('build', ['clean', 'copy-css', 'copy-html', 'bundle']);
+gulp.task('build', ['clean', 'copy-css', 'copy-html', 'bundle', 'sass']);
 
 gulp.task('default', ['build']);

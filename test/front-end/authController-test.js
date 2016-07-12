158667469fb9be0b2dcef8e5fe6cbeedb2edb27c
Ruben Sarkisyan <rubens24@uw.edu>
2016-07-12 15:15:24 -0700

    working on tests

---
+++ test/front-end/authController-test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const angular = require('angular');
+require('angular-mocks');
+require('../../app/js/client.js');
+
+describe('Controller Tests', () => {
+
+  it('should run a test', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('AuthController Test', () => {
+  let ac;
+  let $httpBackend;
+
+  beforeEach(() => {
+    angular.mock.module('BucketListApp');
+    angular.mock.inject(function($controller, _$httpBackend_) {
+      ac = new $controller('AuthController');
+      $httpBackend = _$httpBackend_;
+    });
+  });
+
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingRequest();
+    $httpBackend.verifyNoOutstandingExpectation();
+  });
+
+  it('should allow a user to sign up', () => {
+    $httpBackend.expectPOST('http://localhost:3000/signup')
+      .respond(200, {data: [{body: 'test user'}]});
+
+    ac.signUp();
+    console.log('tag', ac.newUser);
+    $httpBackend.flush();
+
+    expect(ac.newUser.body).toBe('test user');
+  });
+
+  // it('should allow a user to log in', () => {
+  //   $httpBackend.expectGET('http://localhost:8080/login')
+  //     .respond(200, {data: })
+  // })
+});

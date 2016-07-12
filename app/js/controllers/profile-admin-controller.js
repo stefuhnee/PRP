f4164b4004220f9f389fba67d48d91c54ebede03
Kevin Chuang <kcxinkun@gmail.com>
2016-07-12 15:16:14 -0700

    added some profile scripts and logic

--- app/js/controllers/profile-admin-controller.js
+++ app/js/controllers/profile-admin-controller.js
@@ -1,7 +1,18 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('ProfileAdminController', function() {
+  app.controller('ProfileAdminController', ['$http', '$location', 'AuthService', 'EntryService', function($http, $location, AuthService, EntryService) {
 
-  });
+    this.updateProfile = function(user) {
+      return $http({
+        method: 'PUT',
+        data: user,
+        headers: {
+          token: AuthService.getToken();
+        },
+        url: 'http://localhost:3000/'
+      });
+    }
+
+  }]);
 };

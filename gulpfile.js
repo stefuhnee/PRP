8480252a8d3733135fdb6a14782be5f2eaab8083
Stefanie Hansen <stefuhnee@gmail.com>
2016-07-13 11:22:04 -0700

    reorganized project, added app.json including env variable

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,8 @@ const paths = {
   js: __dirname + '/app/**/*.js',
   html: __dirname + '/app/**/*.html',
   css: __dirname + '/app/**/*.css',
-  vendor: __dirname + '/public/**/*.css',
-  img: __dirname + '/public/**/*.png',
-  fonts: __dirname + '/public/**/*.{eot,ttf,woff,svg}'
+  img: __dirname + '/**/*.{png, jpg, gif}',
+  fonts: __dirname + '/**/*.{eot,ttf,woff,svg}'
 };
 
 gulp.task('linter', () => {
@@ -47,8 +46,6 @@ gulp.task('copy-html', ['clean'], () => {
 });
 
 gulp.task('copy-css', ['clean'], () => {
-  gulp.src(paths.vendor)
-    .pipe(gulp.dest('./build'));
   return gulp.src(paths.css)
     .pipe(gulp.dest('./build'));
 });

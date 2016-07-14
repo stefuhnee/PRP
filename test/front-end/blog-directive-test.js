acf40559c46a399279ebe3446615fda1ab4c3aac
Dan Stineback <dstineback@gmail.com>
2016-07-13 21:27:48 -0700

    changed the name of the file. Added directive test but it is not working because it needs the controller added

--- test/front-end/blog-directive-test.js
+++ test/front-end/blog-directive-test.js
@@ -36,4 +36,31 @@ describe('directive test', () => {
 
     expect(li);
   });
+
+  it('should test blog full template', () => {
+    $httpBackend.expectGET('./views/templates/blog-full-view-template.html')
+      .respond(200, blogFullTemplate);
+
+    $scope.test = 'test data';
+    $scope.dateCreated = 'test date';
+    $scope.content = 'test content';
+    let element = angular.element('<blog-full-view-directive title="test" dateCreated="test date" content="test content" ></blog-full-view-directive>');
+    element.data('$ngController', {});
+    let link = $compile(element);
+    let directive = link($scope);
+    $scope.$digest();
+    $httpBackend.flush();
+
+    let h1 = directive.find('h1');
+    let h1Text = h1.text();
+    let h2 = directive.find('h2');
+    let h2Text = h2.text();
+    let p = directive.find('p');
+    let pText = p.text();
+
+    expect(h1Text).toBe('test data');
+    expect(h2Text).toBe('test date');
+    expect(pText).toBe('test content');
+
+  });
 });

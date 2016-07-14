e0d36b2f23d08ccbca9552f13def0adc74ed033c
Stefanie Hansen <stefuhnee@gmail.com>
2016-07-14 13:53:31 -0700

    pretty styling

--- public/bundle.js
+++ public/bundle.js
@@ -45,13 +45,6 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(1);
-	__webpack_require__(7);
-	__webpack_require__(8);
-	__webpack_require__(9);
-	__webpack_require__(12);
-	__webpack_require__(6);
-	__webpack_require__(10);
-	__webpack_require__(11);
 	__webpack_require__(14);
 	__webpack_require__(15);
 	__webpack_require__(16);
@@ -63,7 +56,14 @@
 	__webpack_require__(20);
 	__webpack_require__(21);
 	__webpack_require__(18);
-	module.exports = __webpack_require__(23);
+	__webpack_require__(23);
+	__webpack_require__(7);
+	__webpack_require__(8);
+	__webpack_require__(9);
+	__webpack_require__(12);
+	__webpack_require__(6);
+	__webpack_require__(10);
+	module.exports = __webpack_require__(11);
 
 
 /***/ },

7a52574b60109f17b46308d522c2eab140ac9d0f
Stefanie Hansen <stefuhnee@gmail.com>
2016-07-14 15:22:51 -0700

    thurs-stef-profilestyling

--- public/bundle.js
+++ public/bundle.js
@@ -45,12 +45,6 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(1);
-	__webpack_require__(14);
-	__webpack_require__(15);
-	__webpack_require__(16);
-	__webpack_require__(13);
-	__webpack_require__(17);
-	__webpack_require__(24);
 	__webpack_require__(7);
 	__webpack_require__(8);
 	__webpack_require__(9);
@@ -58,6 +52,12 @@
 	__webpack_require__(6);
 	__webpack_require__(10);
 	__webpack_require__(11);
+	__webpack_require__(14);
+	__webpack_require__(15);
+	__webpack_require__(16);
+	__webpack_require__(13);
+	__webpack_require__(17);
+	__webpack_require__(24);
 	__webpack_require__(22);
 	__webpack_require__(19);
 	__webpack_require__(20);
